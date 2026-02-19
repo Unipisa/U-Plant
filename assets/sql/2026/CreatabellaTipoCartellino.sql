11e995a9b8a7b2f8ea52a6c4fbadd01675df955d
Pietro Picconi <65235437+pietropicconi@users.noreply.github.com>
2026-02-19 11:17:23 +0100

    modificato file sql creazione tipocartellino con aggiunta constrait newid , aggiunto sql per aggiungere campo in tabella individui e aggiungere forekey

--- assets/sql/2026/CreatabellaTipoCartellino.sql
+++ assets/sql/2026/CreatabellaTipoCartellino.sql
@@ -11,9 +11,24 @@ CREATE TABLE [dbo].[TipoCartellino](
  CONSTRAINT [PK_TipoCartellino] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
+ALTER TABLE [dbo].[TipoCartellino] ADD  CONSTRAINT [DF_TipoCartellino_id]  DEFAULT (newid()) FOR [id]
+GO
 INSERT [dbo].[TipoCartellino] ([id], [descrizione], [descrizione_en], [ordinamento]) VALUES (N'61428fdf-2774-4916-9d9e-3746b4a2f23c', N'Non Definito', N'Not Defined', 0)
+GO
+
+ALTER TABLE [dbo].[Individui] ADD [tipocartellino]  uniqueidentifier  Null
+GO
+
+
+Update Individui set tipocartellino  ='61428fdf-2774-4916-9d9e-3746b4a2f23c'
+
+
+ALTER TABLE [dbo].[Individui] ALTER Column [tipocartellino]  uniqueidentifier not Null
+
+
+ALTER TABLE [dbo].[Individui]  WITH CHECK ADD  CONSTRAINT [FK_Individui_TipoCartellino] FOREIGN KEY([tipocartellino])
+REFERENCES [dbo].[TipoCartellino] ([id])
 GO
