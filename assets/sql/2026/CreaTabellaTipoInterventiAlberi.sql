3a38f374e0c7bd58c853c3dd8d44f1623329c0bd
Pietro Picconi <65235437+pietropicconi@users.noreply.github.com>
2026-02-19 11:42:51 +0100

    ho sbagliato e mettere insert nel file sbagliato l'ho spostata in quello giusto

--- assets/sql/2026/CreaTabellaTipoInterventiAlberi.sql
+++ assets/sql/2026/CreaTabellaTipoInterventiAlberi.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[TipoInterventiAlberi](
  CONSTRAINT [PK_InterventiAlberi] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[TipoInterventiAlberi] ADD  CONSTRAINT [DF_InterventiAlberi_id]  DEFAULT (newid()) FOR [id]
@@ -24,3 +24,5 @@ GO
 ALTER TABLE [dbo].[TipoInterventiAlberi] CHECK CONSTRAINT [FK_TipoInterventiAlberi_Organizzazioni]
 GO
 
+INSERT [dbo].[TipoInterventiAlberi] ([id], [descrizione], [descrizione_en], [organizzazione], [ordinamento]) VALUES (N'6cda01e7-afe8-4140-8731-e758d9876f9d', N'Non Definito', N'Not Defined', N'05c0b59a-65ab-4dc4-be25-8a4e3b734587', 0)
+GO
